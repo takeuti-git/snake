2b92cbea64176982ba018c7c8ae21cc6ed5b99b8
takeuti-git <takeuti-git>
2025-11-30 18:17:25 +0900

    fix: main.jsの型安全性を向上

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-// main.js
+// @ts-check
+
 "use strict";
 import { Game } from "./core/game.js";
 import { setupKeyEvents } from "./ui/event.js";
 
 function main() {
     const option = sessionStorage.getItem("map_size");
-    const [width, height] = option ? option.split(",") : [15, 15];
 
-    const canvas = document.getElementById("canvas");
+    const tmp = option ? option.split(",").map(Number) : [15 ,15];
+    const width = tmp[0];
+    const height = tmp[1];
+
+    const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById("canvas"));
     const tileSize = 30;
     canvas.width = width * tileSize;
     canvas.height = height * tileSize;
@@ -18,10 +22,15 @@ function main() {
     setupKeyEvents(game);
 
 }
-main();
 
+/**
+ * @param {number} width 
+ * @param {number} height 
+ */
 globalThis.setMapSize = (width, height = width) => {
     sessionStorage.setItem("map_size", `${width},${height}`);
     console.log(`set mapSize to: ${width},${height}`);
     window.location.href = "./";
 };
+
+document.addEventListener("DOMContentLoaded", main);
